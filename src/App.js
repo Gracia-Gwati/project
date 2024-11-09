baf3145544539f7ac91a6268fc7b67babac7f611
Gracia-Gwati <gracia.gwati@gmail.com>
2024-11-09 18:54:17 +0200

    Updated footer

--- src/App.js
+++ src/App.js
@@ -7,7 +7,14 @@ export default function App() {
     <div className="App">
       <div className="container">
         <Weather defaultCity="Pretoria" />
-        <footer>This project was coded by XXXX</footer>
+        <footer>
+          This project was coded by Tinotenda Gracia Gwati and is open-sourced
+          on{" "}
+          <a href="https://github.com/Gracia-Gwati/project/tree/master">
+            GitHub
+          </a>{" "}
+          and hosted on <a href="https://skyscout-tgg.netlify.app">Netlify</a> .
+        </footer>
       </div>
     </div>
   );
